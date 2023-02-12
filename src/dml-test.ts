6b0def4105cae4d690ac0b583e0a775a2cdba31c
Drew Immerman <drewimmerman@gmail.com>
2023-02-12 13:13:19 -0500

    Add dml tests

---
+++ src/dml-test.ts
@@ -0,0 +1,53 @@
+import 'jasmine';
+import { ColumnType, DatabaseDriverInterface } from 'riao-dbal/src';
+import { TestOptions } from './test-options';
+
+export const dmlTest = (options: TestOptions) =>
+	describe(options.name + ' Query', () => {
+		let conn: DatabaseDriverInterface;
+
+		beforeAll(async () => {
+			conn = new options.driver();
+			await conn.connect(options.connectionOptions);
+			await conn.query(
+				conn.getDataDefinitionBuilder().dropTable({
+					names: 'query_test',
+					ifExists: true,
+				})
+			);
+			await conn.query(
+				conn.getDataDefinitionBuilder().createTable({
+					name: 'query_test',
+					columns: [
+						{
+							name: 'id',
+							type: ColumnType.BIGINT,
+						},
+						{
+							name: 'fname',
+							type: ColumnType.VARCHAR,
+							length: 255,
+						},
+					],
+				})
+			);
+		});
+
+		afterAll(async () => {
+			await conn.disconnect();
+		});
+
+		it('insert rows', async () => {
+			await conn.query(
+				conn.getQueryBuilder().insert({
+					table: 'query_test',
+					records: [
+						{
+							id: 5,
+							fname: 'Test',
+						},
+					],
+				})
+			);
+		});
+	});
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import 'jasmine';
 import { connectionTest } from './connection-test';
 import { ddlTest } from './ddl-test';
+import { dmlTest } from './dml-test';
 import { TestOptions } from './test-options';
 
 export const test = (options: TestOptions) => {
 	connectionTest(options);
 	ddlTest(options);
+	dmlTest(options);
 };
