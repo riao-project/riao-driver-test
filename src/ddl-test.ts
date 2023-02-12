2c308e5cde774381c8f0d2aa94aa2073a3aa453a
Drew Immerman <drewimmerman@gmail.com>
2023-02-12 13:13:05 -0500

    Add DDL tests

---
+++ src/ddl-test.ts
@@ -0,0 +1,38 @@
+import 'jasmine';
+import { ColumnType, DatabaseDriverInterface } from 'riao-dbal/src';
+import { TestOptions } from './test-options';
+
+export const ddlTest = (options: TestOptions) =>
+	describe(options.name + ' Data Definition', () => {
+		let conn: DatabaseDriverInterface;
+
+		beforeAll(async () => {
+			conn = new options.driver();
+			await conn.connect(options.connectionOptions);
+
+			await conn.query(
+				conn.getDataDefinitionBuilder().dropTable({
+					names: ['create_test'],
+					ifExists: true,
+				})
+			);
+		});
+
+		afterAll(async () => {
+			await conn.disconnect();
+		});
+
+		it('can create a table', async () => {
+			await conn.query(
+				conn.getDataDefinitionBuilder().createTable({
+					name: 'create_test',
+					columns: [
+						{
+							name: 'id',
+							type: ColumnType.BIGINT,
+						},
+					],
+				})
+			);
+		});
+	});
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import 'jasmine';
 import { connectionTest } from './connection-test';
+import { ddlTest } from './ddl-test';
 import { TestOptions } from './test-options';
 
 export const test = (options: TestOptions) => {
 	connectionTest(options);
+	ddlTest(options);
 };
