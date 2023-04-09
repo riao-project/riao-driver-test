5f183bcf9fc2d541fc218367c0219d9b6a8048ea
Drew Immerman <drewimmerman@gmail.com>
2023-04-09 06:44:02 -0400

    Add drop table test

---
+++ src/ddl-drop-table-test.ts
@@ -0,0 +1,30 @@
+import 'jasmine';
+import { ColumnType, Database } from 'riao-dbal/src';
+import { TestOptions } from './test-options';
+import { getDatabase } from './init';
+
+export const ddlDropTableTest = (options: TestOptions) =>
+	describe(options.name + ' Data Definition', () => {
+		let db: Database;
+
+		beforeAll(async () => {
+			db = await getDatabase(options);
+		});
+
+		it('can drop a table', async () => {
+			await db.ddl.createTable({
+				name: 'drop_test',
+				ifNotExists: true,
+				columns: [
+					{
+						name: 'id',
+						type: ColumnType.INT,
+					},
+				],
+			});
+
+			await db.ddl.dropTable({
+				names: ['drop_test'],
+			});
+		});
+	});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'jasmine';
 import { connectionTest } from './connection-test';
 import { ddlCreateTableTest } from './ddl-create-table-test';
+import { ddlDropTableTest } from './ddl-drop-table-test';
 import { ddlTruncateTableTest } from './ddl-truncate-table-test';
 import { dmlTest } from './dml-test';
 import { TestOptions } from './test-options';
@@ -8,6 +9,7 @@ import { TestOptions } from './test-options';
 export const test = (options: TestOptions) => {
 	connectionTest(options);
 	ddlCreateTableTest(options);
+	ddlDropTableTest(options);
 	ddlTruncateTableTest(options);
 	dmlTest(options);
 };
