e2aa3cf1b8b6b0bd9d2b4425c302120141668336
Drew Immerman <drewimmerman@gmail.com>
2023-06-03 14:50:37 -0400

    Add integer ColumnType tests

--- src/column-types-test.ts
+++ src/column-types-test.ts
@@ -1,5 +1,5 @@
 import 'jasmine';
-import { ColumnType, Database, QueryRepository } from 'riao-dbal/src';
+import { ColumnType, Database } from 'riao-dbal/src';
 import { TestOptions } from './test-options';
 import { getDatabase } from './init';
 
@@ -11,7 +11,13 @@ export const columnTypesTest = (options: TestOptions) =>
 			db = await getDatabase(options);
 
 			await db.ddl.dropTable({
-				tables: ['bool_column_test'],
+				tables: [
+					'bool_column_test',
+					'tinyint_column_test',
+					'smallint_column_test',
+					'int_column_test',
+					'bigint_column_test',
+				],
 				ifExists: true,
 			});
 		});
@@ -51,5 +57,57 @@ export const columnTypesTest = (options: TestOptions) =>
 
 			expect(falsyRecords.length).toEqual(1);
 			expect(falsyRecords[0].is_a_bool).toBeFalsy();
+		});
+
+		const integerTest = async (
+			columnType: ColumnType,
+			max: number | bigint
+		) => {
+			const table = columnType.toLowerCase() + '_column_test';
+
+			await db.ddl.createTable({
+				name: table,
+				columns: [
+					{
+						type: <any>columnType,
+						name: 'n_numbers',
+					},
+				],
+			});
+
+			await db.query.insert({
+				table,
+				records: [{ n_numbers: max }],
+			});
+
+			const records = await db.query.find({
+				table,
+				where: {
+					n_numbers: max,
+				},
+			});
+
+			if (columnType === ColumnType.BIGINT) {
+				records[0].n_numbers = BigInt(records[0].n_numbers);
+			}
+
+			expect(records.length).toEqual(1);
+			expect(records[0].n_numbers).toEqual(max);
+		};
+
+		it('supports tinyint column', async () => {
+			await integerTest(ColumnType.TINYINT, 127);
+		});
+
+		it('supports small int column', async () => {
+			await integerTest(ColumnType.SMALLINT, 32767);
+		});
+
+		it('supports int column', async () => {
+			await integerTest(ColumnType.INT, 2147483647);
+		});
+
+		it('supports big-int column', async () => {
+			await integerTest(ColumnType.BIGINT, BigInt('9223372036854775807'));
 		});
 	});
