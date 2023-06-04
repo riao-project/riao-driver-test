c987594e3782dc6221d8328f8a16b8b1108c576c
Drew Immerman <drewimmerman@gmail.com>
2023-06-04 09:13:28 -0400

    Add text type column tests

--- src/column-types-test.ts
+++ src/column-types-test.ts
@@ -1,5 +1,5 @@
 import 'jasmine';
-import { ColumnType, Database } from 'riao-dbal/src';
+import { ColumnOptions, ColumnType, Database, like } from 'riao-dbal/src';
 import { TestOptions } from './test-options';
 import { getDatabase } from './init';
 
@@ -23,6 +23,9 @@ export const columnTypesTest = (options: TestOptions) =>
 					'date_column_test',
 					'time_column_test',
 					'datetime_column_test',
+					'char_column_test',
+					'varchar_column_test',
+					'text_column_test',
 				],
 				ifExists: true,
 			});
@@ -305,5 +308,69 @@ export const columnTypesTest = (options: TestOptions) =>
 
 			expect(records.length).toEqual(1);
 			expect(records[0].expiration).toEqual(max);
+		});
+
+		const textTest = async (
+			columnType: ColumnType,
+			options: Partial<ColumnOptions>,
+			strlen: number
+		) => {
+			const table = columnType.toLowerCase() + '_column_test';
+
+			const startCode = 32;
+			const codeRange = 64;
+			let str = '';
+
+			for (let i = 0; i < strlen; i++) {
+				str += String.fromCharCode(
+					Math.round(Math.random() * codeRange) + startCode
+				);
+			}
+
+			await db.ddl.createTable({
+				name: table,
+				columns: [
+					{
+						type: <any>columnType,
+						name: 'text_val',
+						...options,
+					},
+				],
+			});
+
+			await db.query.insert({
+				table,
+				records: { text_val: str },
+			});
+
+			let where = {};
+
+			if (columnType === ColumnType.TEXT) {
+				where = { text_val: like('%') };
+			}
+			else {
+				where = { text_val: str };
+			}
+
+			const records = await db.query.find({
+				table,
+				where,
+			});
+
+			expect(records.length).toEqual(1);
+			expect(records[0].text_val).toEqual(str);
+		};
+
+		it('supports char column', async () => {
+			await textTest(ColumnType.CHAR, {}, 1);
+		});
+
+		it('supports varchar column', async () => {
+			const length = 8000;
+			await textTest(ColumnType.VARCHAR, { length }, length);
+		});
+
+		it('supports text column', async () => {
+			await textTest(ColumnType.TEXT, {}, 65535);
 		});
 	});
