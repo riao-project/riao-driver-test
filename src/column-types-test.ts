7a3824c5ce794f83b3afd2beede3c23a378c185a
Drew Immerman <drewimmerman@gmail.com>
2023-06-04 16:50:09 -0400

    Add blob column test

--- src/column-types-test.ts
+++ src/column-types-test.ts
@@ -26,6 +26,7 @@ export const columnTypesTest = (options: TestOptions) =>
 					'char_column_test',
 					'varchar_column_test',
 					'text_column_test',
+					'blob_column_test',
 				],
 				ifExists: true,
 			});
@@ -372,5 +373,44 @@ export const columnTypesTest = (options: TestOptions) =>
 
 		it('supports text column', async () => {
 			await textTest(ColumnType.TEXT, {}, 65535);
+		});
+
+		it('supports blob column', async () => {
+			const table = 'blob_column_test';
+
+			const strlen = 255;
+			const startCode = 0;
+			const codeRange = 65535;
+			let str = '';
+
+			for (let i = 0; i < strlen; i++) {
+				str += String.fromCharCode(
+					Math.round(Math.random() * codeRange) + startCode
+				);
+			}
+
+			const buf = Buffer.from(str);
+
+			await db.ddl.createTable({
+				name: table,
+				columns: [
+					{
+						type: ColumnType.BLOB,
+						name: 'bin_val',
+					},
+				],
+			});
+
+			await db.query.insert({
+				table,
+				records: { bin_val: buf },
+			});
+
+			const records = await db.query.find({ table });
+
+			const returnedBuffer = Buffer.from(records[0].bin_val);
+
+			expect(records.length).toEqual(1);
+			expect(returnedBuffer).toEqual(buf);
 		});
 	});
